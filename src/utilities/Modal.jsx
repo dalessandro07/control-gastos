4d9947f24eda36f9fd926cc850d9401a72f4b211
Alessandro <drios28@outlook.es>
2022-05-29 16:18:53 -0500

    cambios de estilo y compartir por whatsapp agregado

--- src/utilities/Modal.jsx
+++ src/utilities/Modal.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react'
 
-const Modal = ({ textButtonModal, titleModal, paragraphModal, callbackButtonConfirm }) => {
+const Modal = ({
+  textButtonModal,
+  titleModal,
+  paragraphModal,
+  callbackButtonConfirm,
+  icon,
+  bgc,
+  textButtonConfirm,
+  moreElements
+}) => {
   const [showModal, setShowModal] = useState(false)
 
   return (
     <>
       <button
-        className="flex items-center rounded-sm bg-red-500 p-2 text-sm font-bold text-white hover:bg-red-700"
+        className={`${
+          bgc ?? 'bg-red-500 hover:bg-red-700'
+        } flex items-center rounded-sm p-2 text-sm font-bold text-white `}
         type="button"
         onClick={() => setShowModal(true)}>
         {textButtonModal}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="ml-2 mb-1 h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
+        {icon ?? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="ml-2 mb-1 h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor">
+            <path
+              fillRule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
       </button>
       {showModal ? (
         <>
@@ -40,6 +53,9 @@ const Modal = ({ textButtonModal, titleModal, paragraphModal, callbackButtonConf
                 <div className="relative flex-auto p-6">
                   <p className="my-4 text-lg leading-relaxed text-slate-500">{paragraphModal}</p>
                 </div>
+
+                <div>{moreElements ?? ''}</div>
+
                 <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
                   <button
                     className="background-transparent mr-1 mb-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear hover:underline focus:outline-none"
@@ -54,7 +70,7 @@ const Modal = ({ textButtonModal, titleModal, paragraphModal, callbackButtonConf
                       callbackButtonConfirm()
                       setShowModal(false)
                     }}>
-                    Sí, estoy seguro
+                    {textButtonConfirm ?? 'Sí, estoy seguro'}
                   </button>
                 </div>
               </div>
