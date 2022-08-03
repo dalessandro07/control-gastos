68289f23396b7c1b500b132dee5edc26d87f7759
Alessandro <drios28@outlook.es>
2022-08-03 17:31:31 -0500

    fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,6 @@ import { Routes, Route } from 'react-router-dom'
 import SaldoProvider from '../context/SaldoContext'
 import DivisasProvider from '../context/DivisasContext'
 
-import ProtectedRoute from './app/user/ProtectedRoute'
-
 import Login from './login/Login'
 import PhoneNumberLogin from './login/PhoneNumberLogin'
 import ForgotPassword from './login/ForgotPassword'
@@ -14,8 +12,9 @@ import Register from './register/Register'
 import useResizeWindow from '../hooks/useResizeWindow'
 import ScreenSize from './error/ScreenSize'
 
-import Header from './app/layout/Header'
-import Footer from './app/layout/Footer'
+const Header = lazy(() => import('./app/layout/Header'))
+const Footer = lazy(() => import('./app/layout/Footer'))
+const ProtectedRoute = lazy(() => import('./app/user/ProtectedRoute'))
 
 const Gastos = lazy(() => import('./app/gastos/Gastos'))
 const Saldo = lazy(() => import('./app/saldo/Saldo'))
